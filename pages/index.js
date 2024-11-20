569c54fdb51301999aee96691e7ff84446ef279b
Imran Ashraf <106822824+SAM8402@users.noreply.github.com>
2024-11-20 19:36:24 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,11 @@
-import React from "react";
+import React, { useEffect, useContext, useState } from "react";
+// INTERNAL IMPORT
+import { Pledge_Fund_Context } from ". /Context/Pledge_Fund";
+import { Hero, Card, PupUp } from ". /Components";
+const index = () =>
+const 1
+titleData,
+getCampaigns,
 
 const index = () =>{
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>
