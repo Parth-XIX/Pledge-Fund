c343c58e894b6c570e937f8940f7750049ec33f3
Imran Ashraf <106822824+SAM8402@users.noreply.github.com>
2024-11-22 16:29:51 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,65 @@
 import React, { useEffect, useContext, useState } from "react";
 // INTERNAL IMPORT
-import { Pledge_Fund_Context } from ". /Context/Pledge_Fund";
-import { Hero, Card, PopUp } from ". /Components";
-const index = () =>
-    const{
-      titleData,
-      getCampaigns,
-      createCampaign,
-      donate,
-      getUserCampaigns,
-      getDonations,
-}= useContext (Pledge_Fund_Context);
-const [allcampaign, setAllcampaign] = useState();
-const [usercampaign, setUsercampaign] = useState();
-useEffect ( () => {
-  const getCampaignsData = getCampaigns();
-  const userCampaignsData = getUserCampaigns();
-  return async()=>{
-    const allData = await getCampaignsData;
-    const userData = await userCampaignsData;
-    setAllcampaign(allData);
-    setUsercampaign(userData);
-  };
-},[]);
+import { Pledge_Fund_Context } from "../Context/Pledge_Fund.js";
+import { Hero, Card, PopUp } from "../Components";
 
-//DONATE POPUP MODEL
-const [openModel, setOpenModel] = useState(false) ;
-const [donateCampaign, setDonateCampaign] = useState();
-console.log (donateCampaign) ;
-return (
-<Hero titleData={titleData} createCampaign={createCampaign} />
-<Card
-  title="All Listed Campaign"
-  allcampaign={allcampaign}
-  setOpenModel={setOpenModel}
-  setDonate={setDonateCampaign}
-/>
-<Card
-  title="Your Created Compaign"
-  allcampaign={usercampaign}
-  setOpenMode l={setOpenModel}
-  setDonate={setDonateCampaign}
-/>
-    {openModel && (
-      <PopUp
+const Index = () => {
+  const {
+    titleData,
+    getCampaigns,
+    createCampaign,
+    donate,
+    getUserCampaigns,
+    getDonations,
+  } = useContext(Pledge_Fund_Context);
+
+  const [allCampaign, setAllCampaign] = useState([]);
+  const [userCampaign, setUserCampaign] = useState([]);
+
+  useEffect(() => {
+    const fetchCampaignData = async () => {
+      try {
+        const allData = await getCampaigns();
+        const userData = await getUserCampaigns();
+        setAllCampaign(allData);
+        setUserCampaign(userData);
+      } catch (error) {
+        console.error("Error fetching campaigns:", error);
+      }
+    };
+
+    fetchCampaignData();
+  }, [getCampaigns, getUserCampaigns]);
+
+  // DONATE POPUP MODEL
+  const [openModel, setOpenModel] = useState(false);
+  const [donateCampaign, setDonateCampaign] = useState(null);
+
+  return (
+    <>
+      <Hero titleData={titleData} createCampaign={createCampaign} />
+      <Card
+        title="All Listed Campaigns"
+        allcampaign={allCampaign}
         setOpenModel={setOpenModel}
-        getDonations={getDonations}
-        donate={donateCampaign}
-        donateFunction={donate}
-     />
-    )}
-  </>
+        setDonate={setDonateCampaign}
+      />
+      <Card
+        title="Your Created Campaigns"
+        allcampaign={userCampaign}
+        setOpenModel={setOpenModel}
+        setDonate={setDonateCampaign}
+      />
+      {openModel && (
+        <PopUp
+          setOpenModel={setOpenModel}
+          getDonations={getDonations}
+          donate={donateCampaign}
+          donateFunction={donate}
+        />
+      )}
+    </>
   );
 };
-export default index;
+
+export default Index;
